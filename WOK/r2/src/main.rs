16d4ffb10c3087c10e4f72f4fb0a0c1e7dd7ab43
liqowomo <123171512+liqowomo@users.noreply.github.com>
2024-10-02 09:32:10 +0000

    🫦

---
+++ WOK/r2/src/main.rs
@@ -0,0 +1,8 @@
+mod ut {
+    pub mod ban;
+}
+
+fn main() {
+    println!("Hello, world!");
+    ut::ban::ban();
+}
--- WOK/w1/src/main.rs
+++ WOK/w1/src/main.rs
@@ -1,8 +1,8 @@
-// Testing rust things general
-
-mod uti;
-use uti::*;
+mod uti {
+    pub mod ban;
+}
 
 fn main() {
     println!("Hello, world!");
+    uti::ban::ban1(); // Call the ban1 function from ban.rs file using the ban sub-module within the uti module.
 }
